461f4c48396593a9efcf7c9ec245ced0e8182409
Sylvn001 <juniorbaarros@gmail.com>
2022-04-25 17:45:29 -0300

    feat: config dependencies and created users model and schemas

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from 'src/users/schemas/users.schema';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
-@Module({})
+
+@Module({
+  imports : [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      }
+    ]),
+    AuthModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRATION
+      }
+    })
+  ],
+  providers: [AuthService],
+  exports: [AuthService]
+})
 export class AuthModule {}
